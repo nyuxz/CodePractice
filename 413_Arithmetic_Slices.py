a37a88c8ccf812b4d4b9a5d740d1f9a9321a35bc
Sean <xz1757@nyu.edu>
2017-06-11 21:36:22 -0400

    413

---
+++ 413_Arithmetic_Slices.py
@@ -0,0 +1,20 @@
+#413. Arithmetic Slices
+
+class Solution(object):
+    def numberOfArithmeticSlices(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        @logit: dp
+        dp[i] save the number of slices at ith location
+        """
+        dp = [0] * len(A)
+        count = 0
+        
+        for i in range(2, len(A)):
+            if A[i-1] - A[i-2] == A[i] - A[i-1]:
+                dp[i] = dp[i-1] + 1 
+                
+            count += dp[i]
+        
+        return count
