f980a458b746cefc7d117f0b34cffa02cd87960c
Sean <xz1757@nyu.edu>
2017-06-12 21:16:38 -0400

    389_Find_the_Difference.py

---
+++ 389_Find_the_Difference.py
@@ -0,0 +1,29 @@
+# 389. Find the Difference
+import collections
+
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        diff = list((collections.Counter(t) - collections.Counter(s)))
+
+        return diff[0]
+
+
+    def findTheDifference2(self, s, t):
+    	ss = sorted(s)
+    	tt = sorted(t)
+
+    	diff = list(set(ss) - set(tt))
+
+    	if diff == []:
+    		diff = list(set(tt) - set(ss))
+
+    	return diff[0]
+
+
+print(Solution().findTheDifference("abcd", "abcde"))
+print(Solution().findTheDifference2("abcd", "abcde"))
