cb17a07be7e31ca1c3411d8d301b9d703fb12be5
Sean <xz1757@nyu.edu>
2017-06-25 10:38:23 -0400

    415

--- 415_Add_Strings.py
+++ 415_Add_Strings.py
@@ -1,6 +1,8 @@
 # 415. Add Strings
 
 '''
+Detial about question:
+
 '12345' + '6780'
 
   12345
@@ -22,15 +24,26 @@ class Solution(object):
         :type num1: str
         :type num2: str
         :rtype: str
+
+        @logit:
+        1. convert string to integer, and put in a list in reverse order
+        2. add two list based on integer position in the list. 
+        3. using carry
+
         """
+
+        # convert string to integers using built-in function ord (string --> ASCII --> integer)
         def convertInt(str_num):
             return ord(str_num) - ord('0')
-            
+        
+        # map string to a integer list in reverse order
         num1, num2 = list(map(convertInt, num1[::-1])), list(map(convertInt, num2[::-1]))
         
+        # for loop purpose 
         if len(num1)<len(num2):
             num1, num2 = num2, num1
-            
+        
+       	# deal with carry    
         carry = 0
         for i in range(len(num1)):
             n = num2[i] if i<len(num2) else 0 
@@ -42,9 +55,39 @@ class Solution(object):
             num1.append(1)
         
         return ''.join(map(str, num1))[::-1]
+
+
+    def addStrings_2(self, num1, num2):
+    	'''
+    	@logit: using itertools.izip_longest
+    	1. izip_longest: combine items to tuple generator
+    	2. still need reverse order of items list
+    	'''
+    	carry = 0 
+    	for (x, y) in izip_longest(num1[::-1], num2[::-1], fillvalue='0'):
+    		tmp = (int(x) + int(y) + carry)
+    		reminder = tmp % 10
+    		carry = tmp // 10
+
+    		# reverse back happends here
+    		# if result = result + str(reminder), will need reverse string again
+    		result = str(reminder) + result
+
+    		if carry:
+            	result = str(carry) + result
+
+    	return result
+
+
+
+
+
+
+
             
 if __name__ == '__main__':
 	print (Solution().addStrings('1456','934'))
+	print (Solution().addStrings_2('1456','934'))
 
 
 '''
@@ -54,4 +97,10 @@ e.g. convert string '0' to interger 48
 ord('0') = 48 
 ord('52') = 52
 ord('4') - ord('0') = 4
+
+P.s.s
+itertools.izip_longest('ABCD', 'xy', fillvalue='-') --> Ax By C- D-
+
 '''
+
+
