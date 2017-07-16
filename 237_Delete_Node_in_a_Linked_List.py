9e8a8a13dd7d07e2d7c4df8956b0ea0a676b38d2
Sean <xz1757@nyu.edu>
2017-07-15 21:27:12 -0400

    237

--- 237_Delete_Node_in_a_Linked_List.py
+++ 237_Delete_Node_in_a_Linked_List.py
@@ -6,88 +6,35 @@ class ListNode(object):
         self.val = x
         self.next = None
 
+    def __repr__(self):
+        if self:
+            return "{} -> {}".format(self.val, repr(self.next))
+
 class Solution(object):
-    
-    def __init__(self):
-	    self.head = None
-
-    # Function to insert a new node at the beginning
-    def push(self, new_data):
-        new_node = ListNode(new_data)
-        new_node.next = self.head
-        self.head = new_node
-
-    # Utility function to print the linked LinkedList
-    def printList(self):
-        temp = self.head
-        while(temp):
-            print (" %d " %(temp.val),)
-            temp = temp.next
-
     def deleteNode_2(self, node):
         """
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
         """
-        node = self.head 
         node.val = node.next.val
         node.next = node.next.next
-        
-    def deleteNode(self, position):
- 
-        # If linked list is empty
-        if self.head == None:
-            return
- 
-        # Store head node
-        temp = self.head
- 
-        # If head needs to be removed
-        if position == 0:
-            self.head = temp.next
-            temp = None
-            return
- 
-        # Find previous node of the node to be deleted
-        for i in range(position -1 ):
-            temp = temp.next
-            if temp is None:
-                break
- 
-        # If position is more than number of nodes
-        if temp is None:
-            return
-        if temp.next is None:
-            return
- 
-        # Node temp.next is the node to be deleted
-        # store pointer to the next of node to be deleted
-        next = temp.next.next
- 
-        # Unlink the node from linked list
-        temp.next = None
- 
-        temp.next = next
-
-if __name__ == '__main__':
-    # create linked list
-    llist = Solution()
-    llist.push(7)
-    llist.push(1)
-    llist.push(3)
-    llist.push(2)
-    llist.push(8)
-    print ("Created Linked List: ")
-    llist.printList()
 
 
-    llist.deleteNode(2)
-    print ("\nLinked List after Deletion at position 2: ")
-    llist.printList()
 
-    llist.deleteNode_2(2)
-    print ("\nLinked List after Deletion at position 2: ")
-    llist.printList()
+
+if __name__ == "__main__":
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    head.next.next.next.next = ListNode(5)
+
+    print ('initial:', head)
+    print (Solution().deleteNode_2(2))
+
+
+
+
 
 '''
 ToDo:
