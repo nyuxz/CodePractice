a64148ea1372269478ff337a9b27149888934e31
Sean <xz1757@nyu.edu>
2017-07-16 21:53:33 -0400

    234

--- 234_Palindrome_Linked_List.py
+++ 234_Palindrome_Linked_List.py
@@ -14,6 +14,7 @@ class Solution(object):
         @logit:
        		1. save linked list value to a empty list
        		2. check if this list is palindrome 
+       		3. time: O(n), space: O(n)
         """
 
         # empty linked list is palindrome
@@ -32,3 +33,59 @@ class Solution(object):
        			return False
 
        	return True
+
+
+    def isPalindrome_2(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        @logit:
+       		1. use fast and slow pointer to find the center position
+       		2. fast pointer is twice speed of slow pointer, so that when faster pointer
+       		reach None, slow pointer at the middle of the linked list
+       		3. reverse second half linked list to compare with the first half
+       		4. ignore the even/odd length of the linked list, coz slow pointer will alway point
+       		to second half linked list.      	
+        """
+
+        if not head or not head.next:
+            return True
+
+        fast = slow = head
+
+        # fast pointer move two position each time
+        # if odd length llist, stop at fast.next.next == None
+        # if even length llist, stop at fastnext == None
+       	while fast.next and fast.next.next: # pay attention here !!
+       		fast = fast.next.next
+       		slow = slow.next
+
+       	# make slow pointer alway point to second half of linked list
+       	slow = slow.next
+
+       	# reverse second half linked list
+       	slow = self.reverseList(slow)
+
+       	while slow:
+       		if head.val != slow.val: # we are comparing with the value only
+       			return False
+
+       		slow = slow.next
+       		head = head.next
+
+       	return True 
+
+    def reverseList(self, head):
+        new_head = None
+        while head:
+            p = head
+            head = head.next
+            p.next = new_head
+            new_head = p
+        return new_head
+
+
+
+
+
+
