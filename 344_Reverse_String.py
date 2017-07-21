042c51a5b7af98750feecc52afcf5878e76e3cab
Sean <xz1757@nyu.edu>
2017-07-21 11:06:54 -0400

    344

--- 344_Reverse_String.py
+++ 344_Reverse_String.py
@@ -1,11 +1,11 @@
 # 344. Reverse String
+import math 
 
 class Solution(object):
     def reverseString(self, s):
         """
         :type s: str
         :rtype: str
-
         @logit: slice
         """
         return s[::-1]
@@ -24,6 +24,40 @@ class Solution(object):
 
     	return result
 
+    def reverseString_4(self, s):
+        '''
+        @logit: Recursion
+        '''
+        if len(s) <= 1:
+            return s
+
+        left_s = s[:len(s)//2]
+        right_s = s[len(s)//2:]
+        return self.reverseString_4(right_s) + self.reverseString_4(left_s)
+
+    def reverseString_5(self,s):
+        '''
+        @logit:
+            1. this is the very traditional way to reverse a list/string/integer
+            2. 暴力
+        '''
+        s = list(s)
+
+        for i in range(0, len(s)//2):
+            tmp = s[i]
+            s[i] = s[len(s) -1 - i]
+            s[len(s) -1 - i] = tmp
+
+        return ''.join(s)
+
+
 print (Solution().reverseString('abcdefg'))
 print (Solution().reverseString_2('abcdefg'))
 print (Solution().reverseString_3('abcdefg'))
+print (Solution().reverseString_4('abcdefg'))
+print (Solution().reverseString_5('abcdefg'))
+
+
+
+
+
