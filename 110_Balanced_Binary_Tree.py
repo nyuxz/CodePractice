2182670211110f315991900e9cc1bbb9b4fbdc05
Sean <xz1757@nyu.edu>
2017-07-23 19:19:49 -0400

    110

--- 110_Balanced_Binary_Tree.py
+++ 110_Balanced_Binary_Tree.py
@@ -14,6 +14,7 @@ class Solution(object):
         :rtype: bool
         @logit: 
         	1. traditional way
+        	2. inefficient, coz need to calculate the depth of some sub-nodes multiple times.
         """
     	if root == None:
        		return True
@@ -28,7 +29,71 @@ class Solution(object):
 
     def getDepth(self, root):
         if root == None:
-            return 0
+            return 0 
         else:
             return 1 + max(self.getDepth(root.left), self.getDepth(root.right))
             
+
+class Solution_2(object):
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        @logit: 
+        	1. much efficient than the solution 1,
+        	coz for each node, we only calculate the depth once, no repeat extra calculations.
+        	2. define the function to get the depth of all nodes in one times, and save the depth into .val
+        """
+    	if root == None:
+       		return True
+	
+		# get the depth of all nodes and save the values
+        self.getAllDepth(root) 
+
+        left_depth = root.left.val if root.left else 0 # should give None type a value of 0
+        right_depth = root.right.val if root.right else 0 
+
+        if abs(left_depth - right_depth) <= 1:
+            return self.isBalanced(root.left) and self.isBalanced(root.right)
+        else:
+            return False
+
+    def getAllDepth(self, root):
+        if root == None:
+            return 0 # if [root.val = 0] will have error: 'NoneType' object has no attribute 'val'
+        else: 
+            root.val = 1 + max(self.getAllDepth(root.left), self.getAllDepth(root.right))
+        
+        return root.val
+
+class Solution_3(object):
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        @logit: 
+        	1. DFS traversal the tree, and just record if the tree is balance or not, save the space without record depth
+            2. using -1 to indicate the tree is unbalance
+  
+        """
+        return self.dfs(root) != -1
+    
+    def dfs(self, root):
+        
+    	if root == None:
+       		return True
+        
+        left_depth = self.dfs(root.left)
+        if left_depth == -1:
+            return -1
+        
+        right_depth = self.dfs(root.right)
+        if right_depth == -1:
+            return -1
+        
+        if abs(left_depth - right_depth) <= 1:
+            return 1 + max(left_depth, right_depth)
+        else:
+            return -1
+
+
