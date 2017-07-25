847a7618857b27c52adae7c49e3a7dae4c839053
Sean <xz1757@nyu.edu>
2017-07-25 09:06:59 -0400

    226

--- 226_Invert_Binary_Tree.py
+++ 226_Invert_Binary_Tree.py
@@ -24,3 +24,75 @@ class Solution(object):
         root.right = self.invertTree(tmp)
         
         return root
+
+
+class Solution_2(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        @logic: Stack
+            1. Tree traversal without using recursion, we use stack instead.
+            2. stack =[] almost like a list in python data structure  
+                *stack.pop(): get last element and del it from current list
+                *stack.append(): adding new element just like what list did
+        """
+        if root == None:
+            return root
+        
+        #define a sack
+        stack = [root]
+        
+        while len(stack) != 0:
+            node = stack.pop() #pop up last element of the stack
+            
+            #invert
+            node.left, node.right = node.right, node.left
+            
+            #attention here! -> append after invert 
+            if node.left:
+                stack.append(node.left) #append at the last of the stack
+            if node.right:
+                stack.append(node.right)
+                
+        return root
+
+class Solution_3(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        @logic: BFS, Queue
+            1. this way kind same with using stack
+            2. instead of pop up last element each time, we take head of the queue
+            3. also, new child node will put into last of the queue
+            4. each time after invert child node, we should delete current node.
+        
+        @note:
+            1. why if i define node = queue[0], and then using node.left instead of using queue[0].left, cannot AC.
+            saying: Time Limit Exceeded
+        """
+        if root == None:
+            return root
+        
+        # define a queue
+        queue = [root]
+        
+        while len(queue) != 0:
+            
+            #node = queue[0]
+            
+            #invert
+            queue[0].left, queue[0].right = queue[0].right, queue[0].left
+            
+            #attention here! -> append after invert 
+            if queue[0].left:
+                queue.append(queue[0].left) 
+            if queue[0].right:
+                queue.append(queue[0].right)
+            
+            del queue[0]
+                
+        return root
+
+    
