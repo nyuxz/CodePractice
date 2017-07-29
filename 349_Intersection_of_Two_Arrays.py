cf53ebc7469c1ed9a8294d73cbc8e0d19e135f4d
Sean <xz1757@nyu.edu>
2017-07-29 00:23:46 -0400

    349

--- 349_Intersection_of_Two_Arrays.py
+++ 349_Intersection_of_Two_Arrays.py
@@ -25,3 +25,57 @@ class Solution_2(object):
                 intersection.append(i)
                 
         return intersection
+
+
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        @logic: 暴力
+        """
+        itec_list = []
+        map_dict = {}
+
+        for i in nums1:
+        	if i in map_dict:
+        		map_dict[i] += 1
+        	else:
+        		map_dict[i] = 1
+
+        for j in nums2:
+        	if j in map_dict:
+        		map_dict[j] += 1
+        	else:
+        		map_dict[i] = 1
+
+        for k in map_dict and map_dict[k] > 1:
+        	itec_list.append(k)
+
+        return itec_list
+
+class Solution_3(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        @logic: dict
+            1. the benifit: dict will never double count same [key, value]
+            e.g. dict[a] = 1 -> {a:1}
+            if we add another same key and value dict[a] = 1, the dict keep same as {a:1}
+            however, if the list, will keep append whenever same elements or not
+        """
+        itec_dict = {}
+        map_dict = {}
+
+        for i in nums1:
+        	map_dict[i] = 1
+    
+        for j in nums2:
+        	if j in map_dict:
+        		itec_dict[j] = 1
+                
+        return list(itec_dict.keys())
+
