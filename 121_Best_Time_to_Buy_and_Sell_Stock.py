b74b8fcaa92bb9bf4f94a576399122010c0929f6
Sean <xz1757@nyu.edu>
2017-08-03 23:20:25 -0400

    121

--- 121_Best_Time_to_Buy_and_Sell_Stock.py
+++ 121_Best_Time_to_Buy_and_Sell_Stock.py
@@ -1,4 +1,5 @@
 # 121. Best Time to Buy and Sell Stock
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
 
 class Solution(object):
     def maxProfit(self, prices):
@@ -17,3 +18,20 @@ class Solution(object):
             max_profit = max(max_profit, profit)
             
         return max_profit
+
+
+class Solution_2(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        @logic: DP
+        """
+        if len(prices) == 0:
+            return 0
+        min_Price = prices[0]
+        dp = [0] * len(prices)
+        for i in range(0, len(prices)):
+            dp[i] = max(dp[i-1], prices[i] - min_Price)
+            min_Price = min(min_Price, prices[i])
+        return dp[-1]
