5831f7b95569db9996e116153d9f8d2b2bc82adf
Sean <xz1757@nyu.edu>
2017-08-10 23:12:34 -0400

    107

---
+++ 107_Binary_Tree_Level_Order_Traversal_II.py
@@ -0,0 +1,45 @@
+# 107. Binary Tree Level Order Traversal II
+# https://leetcode.com/problems/binary-tree-level-order-traversal-ii/description/
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        res = []
+        queue = [(root, 0)]
+        while len(queue) > 0:
+            node, depth = queue.pop()
+            if node:
+                if len(res) <= depth:
+                    res.insert(0, [])
+                res[-(depth+1)].append(node.val)
+                queue.insert(0, (node.left, depth+1))
+                queue.insert(0, (node.right, depth+1))
+        return res
+
+class Solution_2(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        res = []
+        stack = [(root, 0)]
+        while len(stack) > 0:
+            node, depth = stack.pop()
+            if node:
+                if len(res) <= depth:
+                    res.insert(0, [])
+                res[-(depth+1)].append(node.val)
+                stack.append((node.right, depth+1))
+                stack.append((node.left, depth+1))
+        return res
