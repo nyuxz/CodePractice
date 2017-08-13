3d5289a124122163e0bcd9aaaba3f78ce7187b20
Sean <xz1757@nyu.edu>
2017-08-12 20:00:55 -0400

    107

--- 107_Binary_Tree_Level_Order_Traversal_II.py
+++ 107_Binary_Tree_Level_Order_Traversal_II.py
@@ -13,18 +13,31 @@ class Solution(object):
         """
         :type root: TreeNode
         :rtype: List[List[int]]
+        @logic: BFS
         """
-        res = []
+        # result is a list of list
+        result = []
+        
+        # queue record the node and depth
         queue = [(root, 0)]
-        while len(queue) > 0:
+        
+        while len(queue) > 0: 
+            
             node, depth = queue.pop()
+            
             if node:
-                if len(res) <= depth:
-                    res.insert(0, [])
-                res[-(depth+1)].append(node.val)
+                # if we move to next depth 
+                if len(result) <= depth:
+                    # we will create a new list of list to record node.val
+                    result.insert(0, [])
+                # append node.val at last list of result list   
+                result[-(depth+1)].append(node.val)
+                
                 queue.insert(0, (node.left, depth+1))
                 queue.insert(0, (node.right, depth+1))
-        return res
+        
+        return result
+                
 
 class Solution_2(object):
     def levelOrderBottom(self, root):
@@ -50,15 +63,19 @@ class Solution_3(object):
         """
         :type root: TreeNode
         :rtype: List[List[int]]
+        @recursion
         """
-        res = []
-        self.dfs(root, 0, res)
-        return res
+        result = []
+        self.dfs(root, 0, result)
+        return result
 
-    def dfs(self, root, depth, res):
+    def dfs(self, root, depth, result):
         if root:
-            if depth >= len(res):
-                res.insert(0, [])
-            res[-(depth+1)].append(root.val)
-            self.dfs(root.left, depth+1, res)
-            self.dfs(root.right, depth+1, res)
+            if depth >= len(result):
+                result.insert(0, [])
+            result[-(depth+1)].append(root.val)
+            self.dfs(root.left, depth+1, result)
+            self.dfs(root.right, depth+1, result)
+
+
+
