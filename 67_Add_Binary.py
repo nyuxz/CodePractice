374af66e9cf42d7c0764bfa151deb5bbfc3a76db
Sean <xz1757@nyu.edu>
2017-09-26 21:35:05 -0400

    67

--- 67_Add_Binary.py
+++ 67_Add_Binary.py
@@ -16,3 +16,28 @@ class Solution(object):
         			bin(15)[2:] --> 1111
         """
         return bin(int(a, 2) + int(b, 2))[2:]
+
+
+class Solution_2(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        @logic: Recursion
+            1. a[:-1] takes the string except last character  
+        """
+
+        if not a or not b:
+            return a if a else b
+
+        if a[-1] == '1' and b[-1] == '1':
+            return self.addBinary(self.addBinary(a[:-1], b[:-1]), '1') + '0'
+
+        elif a[-1] == '0' and b[-1] == '0':
+            return self.addBinary(a[:-1], b[:-1]) + '0'
+            
+        else:
+            return self.addBinary(a[:-1], b[:-1]) + '1'
+
+
